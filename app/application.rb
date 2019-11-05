436da7bb5d20734bb968fa2c06acd545e35a67e7
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-05 19:55:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
       item = req.params["item"]
       if @@items.include?(item)
         @@cart << item 
-        resp.write "Added #{item}"
+        resp.write "added #{item}"
       else 
         resp.write "Error"
       end 
